a4c359494828083ff969d75b5f6431ec8acb8861
Alina Bychkova <ialina240200@gmail.com>
2022-03-25 23:04:27 +0300

    Move typography

--- shared/components/Typography/Typography.tsx
+++ shared/components/Typography/Typography.tsx
@@ -1,11 +1,11 @@
-import { Colors } from "./Colors";
+import { Colors } from "../../constants/Colors";
 import styled from "styled-components";
 
 export const Title = styled.span`
   font-family: "Suisse Intl Book";
   font-size: 24px;
   line-height: 24px;
-  letter-spacing: -0.015;
+  letter-spacing: -0.015em;
   color: ${Colors.Main.Gray_3};
 `;
 
@@ -13,7 +13,7 @@ export const Headline = styled.span`
   font-family: "Suisse Intl Book";
   font-size: 18px;
   line-height: 20px;
-  letter-spacing: -0.015;
+  letter-spacing: -0.015em;
   color: ${Colors.Main.Gray_3};
 `;
 
@@ -21,7 +21,7 @@ export const Subtitle = styled.span`
   font-family: "Suisse Intl";
   font-size: 18px;
   line-height: 20px;
-  letter-spacing: -0.015;
+  letter-spacing: -0.015em;
   font-weight: 600;
   color: ${Colors.Main.Gray_3};
 `;
---
+++ shared/components/index.ts
@@ -0,0 +1,9 @@
+export {
+  Title,
+  Headline,
+  Subtitle,
+  Text,
+  Caption_1,
+  Caption_2,
+  Caption_3,
+} from "./Typography/Typography";
