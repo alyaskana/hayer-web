f87daede5f938af9b8e0d6a73731317e658f6c6f
Alina Bychkova <ialina240200@gmail.com>
2022-03-25 22:06:28 +0300

    Add styled-components and svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            ref: true,
+          },
+        },
+      ],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
